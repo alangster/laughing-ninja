39c34960172771807b8b7beab8c4110fc2cf7b4a
Alex Lang <langalex4@gmail.com>
2015-02-05 12:24:36 -0600

    fixed edit completion bug

--- app/controllers/todosCtrl.js
+++ app/controllers/todosCtrl.js
@@ -13,38 +13,8 @@ function TodosCtrl(Api, TodosCollectionService) {
 
 	this.TodosCollectionService = TodosCollectionService;
 
-	// var makeChart = function() {
-	// 	console.log(TodosCollectionService.todoCounts);
-	// 	var pieChart = {};
-	// 	pieChart.type = "PieChart";
-	// 	pieChart.data = [
-	// 		['something','something else'],
-	// 		['Complete', TodosCollectionService.todoCounts.complete],
-	// 		['Incomplete', TodosCollectionService.todoCounts.incomplete]
-	// 	];
-	// 	pieChart.options = {
- //      displayExactValues: true,
- //      width: 400,
- //      height: 200,
- //      is3D: true,
- //      chartArea: {left:10,top:10,bottom:0,height:"100%"}
- //    };
- //    pieChart.formatters = {
- //      number : [{
- //      	columnNum: 1,
- //      	pattern: "$ #,##0.00"
- //      }]
- //    };
- //    return pieChart;
-	// }
-
-	var that = this;
-	(function init() {
-		Api.fetchTodos().then(function(fetchedTodos) {
-			TodosCollectionService.setTodos(fetchedTodos);
-		}, function() {
-			that.errors.todoFetch = "Experienced a problem loading your todos.";
-		})
+	var makeChart = function() {
+		// console.log(TodosCollectionService.todoCounts);
 		var pieChart = {};
 		pieChart.type = "PieChart";
 		pieChart.data = [
@@ -65,7 +35,17 @@ function TodosCtrl(Api, TodosCollectionService) {
       	pattern: "$ #,##0.00"
       }]
     };
-		that.chart = pieChart;
+    return pieChart;
+	}
+
+	var that = this;
+	(function init() {
+		Api.fetchTodos().then(function(fetchedTodos) {
+			TodosCollectionService.setTodos(fetchedTodos);
+		}, function() {
+			that.errors.todoFetch = "Experienced a problem loading your todos.";
+		})
+		that.chart = makeChart();
 	})();
 
 	var predicate = function(input) {
@@ -98,17 +78,19 @@ function TodosCtrl(Api, TodosCollectionService) {
 		originalTodos[todo.id] = todo;
 	}
 
-	this.editTodo = function(todo, context) {
+	this.editTodo = function(todo) {
 		Api.updateTodo(todo).then(function() {
-			if (context) {
-				TodosCollectionService.adjustCounts(todo);
-			}
 			originalTodos[todo.id] = null;
 		}, function() {
 			todo = originalTodos[todo.id];
 		})
 	}
 
+	this.toggleCompletion = function(todo) {
+		that.saveOriginal(todo);
+		todo.is_complete = !todo.is_complete;
+		that.editTodo(todo);
+	}
 
 
 
